c0988123db6ca14230bbf36968af652e7f5e499c
Zrna <lukazrnic5@gmail.com>
2021-03-20 13:16:55 +0100

    add users migration

---
+++ migrations/20210320120049-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('users');
+  },
+};
