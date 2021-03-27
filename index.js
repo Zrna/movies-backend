7edc97600fdacb5d8d318c203f4649731d10e907
Zrna <lukazrnic5@gmail.com>
2021-03-27 11:47:16 +0100

    add Auth routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 
 const db = require('./models');
+const authRoutes = require('./routes/Auth');
 
 const PORT = 5000;
 const app = express();
@@ -18,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
+app.use(authRoutes);
 
 app.listen(PORT, error => {
   if (error) return console.log(`Cannot listen on PORT: ${PORT}`);
