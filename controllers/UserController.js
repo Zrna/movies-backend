7755848eda15747376b730d2b9bf060c6b24078c
zrna <lukazrnic5@gmail.com>
2021-04-27 15:12:55 +0200

    add PUT `/api/account` endpoint for update user account info

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -24,6 +24,67 @@ const get_account = async (req, res) => {
   });
 };
 
+const update_account = (req, res) => {
+  const accessToken = req.cookies['access-token'];
+  const decodedToken = decode(accessToken);
+  const userId = decodedToken.id;
+
+  const { firstName, lastName } = req.body;
+
+  if (!firstName) {
+    return res.status(422).json({
+      error: "First name can't be empty",
+    });
+  }
+
+  if (!lastName) {
+    return res.status(422).json({
+      error: "Last name can't be empty",
+    });
+  }
+
+  User.update(
+    {
+      firstName,
+      lastName,
+    },
+    {
+      where: {
+        id: userId,
+      },
+    }
+  )
+    .then(() => {
+      User.findOne({
+        where: {
+          id: userId,
+        },
+      })
+        .then(user => {
+          const { id, email, firstName, lastName, createdAt, updatedAt } = user.dataValues;
+
+          return res.status(200).json({
+            id,
+            email,
+            firstName,
+            lastName,
+            createdAt,
+            updatedAt,
+          });
+        })
+        .catch(() =>
+          res.status(500).json({
+            error: 'Something went wrong with updating the account data.',
+          })
+        );
+    })
+    .catch(() =>
+      res.status(500).json({
+        error: 'Update Account data error',
+      })
+    );
+};
+
 const delete_account = (req, res) => {
   const accessToken = req.cookies['access-token'];
   const decodedToken = decode(accessToken);
@@ -51,5 +112,6 @@ const delete_account = (req, res) => {
 
 module.exports = {
   get_account,
+  update_account,
   delete_account,
 };
--- routes/User.js
+++ routes/User.js
@@ -55,14 +55,94 @@ router.get('/api/account', validateToken, UserController.get_account);
 /**
  * @swagger
  * /api/account:
+ *  put:
+ *    tags:
+ *      - account
+ *    summary: Account update
+ *    description: Required `access-token` in cookies
+ *    requestBody:
+ *      description: Update user's account data
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              firstName:
+ *                type: string
+ *                example: Luka
+ *              lastName:
+ *                type: string
+ *                example: Zrnic Updated
+ *    responses:
+ *      200:
+ *        description: User account data updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                  type: number
+ *                  example: 1
+ *                email:
+ *                  type: string
+ *                  example: zrna@gmail.com
+ *                firstName:
+ *                  type: string
+ *                  example: Luka
+ *                lastName:
+ *                  type: string
+ *                  example: Zrnic Updated
+ *                createdAt:
+ *                  type: string
+ *                  example: 2021-04-18T10:02:07.000Z
+ *                updatedAt:
+ *                  type: string
+ *                  example: 2021-04-27T20:00:00.000Z
+ *      401:
+ *        description: No `access-token` in cookies
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  example: Access token is missing
+ *      500:
+ *        description: Account data update error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  example: Something went wrong with updating the account data.
+ */
+router.put('/api/account', validateToken, UserController.update_account);
+
+/**
+ * @swagger
+ * /api/account:
  *  delete:
  *    tags:
  *      - account
- *    summary: Delete account
+ *    summary: Account delete
  *    description: Required `access-token` in cookies
  *    responses:
  *      200:
  *        description: Account deleted - returns `true`
+ *      401:
+ *        description: No `access-token` in cookies
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  example: Access token is missing
  *      500:
  *        description: Server Error
  *        content:
