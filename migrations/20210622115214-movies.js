ab6bfd359049bc49ea2f4862c9a65bbd1f531a13
Zrna <lukazrnic5@gmail.com>
2021-06-22 14:01:33 +0200

    update review column type

--- migrations/20210622115214-movies.js
+++ migrations/20210622115214-movies.js
@@ -17,7 +17,7 @@ module.exports = {
         allowNull: false,
       },
       review: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
       },
       createdAt: Sequelize.DATE,
